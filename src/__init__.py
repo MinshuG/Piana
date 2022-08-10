b3acd19d0423479c5deec394d880c85aa33f3bf8
floxay <57007485+floxay@users.noreply.github.com>
2022-08-10 13:11:19 +0200

    v1.0.10

--- src/__init__.py
+++ src/__init__.py
@@ -19,7 +19,7 @@ bl_info = {
     "author": "Luviana",
     "description": "Tools & Scripts for creating VALORANT Content Creation",
     "blender": (3, 00, 0),
-    "version": (1, 0, 9),
+    "version": (1, 0, 10),
     "location": "3D View > Tools",
     "warning": "",
     "category": "Import-Export",
